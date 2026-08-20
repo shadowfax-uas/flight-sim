b84f2b77208b915d35e524ad33000993efe4b65d
shadowfax-uas <shadowfax.uas@proton.me>
2026-08-20 10:12:36 -0400

    updates for consistency

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,15 @@
 #include "Waypoint.hpp"
 
 int main() {
-    Waypoint waypoint_1{10.0, 20.0, 30.0};
-    Waypoint waypoint_2{15.0, 25.0, 35.0};
-    Waypoint waypoint_3{20.0, 30.0, 40.0};
+    Waypoint waypoint1{10.0, 20.0, 30.0};
+    Waypoint waypoint2{15.0, 25.0, 35.0};
+    Waypoint waypoint3{20.0, 30.0, 40.0};
     Drone drone;
-    drone.flyTo(waypoint_1);
+    drone.flyTo(waypoint1);
     drone.printStatus();
-    drone.flyTo(waypoint_2);
+    drone.flyTo(waypoint2);
     drone.printStatus();
-    drone.flyTo(waypoint_3);
+    drone.flyTo(waypoint3);
     drone.printStatus();
     return 0;
 }
